596e53ba1b8a7855324c1c2fdce6f81ac08a6ea2
maxh <maxhumpherys@gmail.com>
2021-09-23 15:48:48 -0600

    function for randomizing characters, length of randomized characters supplied by user

--- script.js
+++ script.js
@@ -2,7 +2,28 @@
 // lowercase 0-26   uppercase 26-52     numbers 52-62     special characters 62-89
 let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};':<>,./?";
 
+function password() {
+  let characterLength = window.prompt("How many characters would you like?");
+  characterLength = parseInt(characterLength, 10);
+  console.log(characterLength);
+  let password = "";
 
+  while (characterLength < 8 || characterLength > 128) {
+    characterLength = window.prompt("Pleasse chose between 8 and 128 characters");
+  }
+
+
+
+
+  for (let i = 0; i <= characterLength; i++) {
+    var random = Math.floor(Math.random() * characters.length);
+    password += characters.substring(random, random + 1);
+    password = password.substring(0, characterLength);
+    console.log(password);
+  }
+}
+
+password();
 // let generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
