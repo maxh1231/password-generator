210c09d156565f4097b67761d0e6e892ce610d37
maxh <maxhumpherys@gmail.com>
2021-09-23 15:07:51 -0600

    restart again

--- script.js
+++ script.js
@@ -1,48 +1,5 @@
 // Assignment Code
-let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let numericCharacters = "0123456789";
-let specialCharacters = "!@#$%^&*()`~{}[]:;<>,.?/|_-+=";
 
-function passwordOptions() {
-  let characterLength = window.prompt("Please select a length of characters");
-
-  if (characterLength < 8 || characterLength > 128) {
-    confirm("Please select a character lenght between 8-128")
-    passwordOptions();
-
-  }
-
-  let isUpperCase = confirm("Would you like to include uppercase characters?");
-  if (isUpperCase) {
-    var isUpperCaseStr = upperCaseCharacters[Math.floor(Math.random() * upperCaseCharacters.length)];
-
-  }
-
-  let isLowerCase = confirm("Would you like to include lowercase characters?");
-  if (isLowerCase) {
-    var isLowerCaseStr = lowerCaseCharacters[Math.floor(Math.random() * lowerCaseCharacters.length)];
-  }
-
-  let isNumeric = confirm("Would you like to include numbers?");
-  if (isNumeric) {
-    var isNumericStr = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
-  }
-
-  let isSpecial = confirm("Would you like to include special characters?");
-  if (isSpecial) {
-    var isSpecialStr = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-  }
-
-  console.log(characterLength);
-  console.log(isUpperCaseStr);
-  console.log(isLowerCaseStr);
-  console.log(isNumericStr);
-  console.log(isSpecialStr);
-
-}
-
-passwordOptions();
 
 // let generateBtn = document.querySelector("#generate");
 
