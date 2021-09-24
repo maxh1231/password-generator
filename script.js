ff6c53b371ea115aea0ef1cfeda7f8a4aca56a56
maxh <maxhumpherys@gmail.com>
2021-09-23 21:03:20 -0600

    added ONLY uppcase characters option and ONLY special characters option

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 let numbers = "1234567890";
 let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let SpecialCharacters = "!@#$%^&*()_+-=[]{};':<>,./?";
+let specialCharacters = "!@#$%^&*()_+-=[]{};':<>,./?";
 
 function password() {
   let characterLength = window.prompt("How many characters would you like?");
@@ -21,6 +21,8 @@ function password() {
   let isNumeric = confirm("Would you like to include Numeric characters?");
   let isSpecial = confirm("Would you like to include Special Characters?");
 
+
+  // if ONLY lowercase characters are selected
   if (isLowerCase && isUpperCase == false && isNumeric == false && isSpecial == false) {
     for (let i = 0; i <= characterLength; i++) {
       var random = Math.floor(Math.random() * lowerCase.length);
@@ -30,6 +32,17 @@ function password() {
     }
   }
 
+  // if ONLY Uppercase characters are selected
+  if (isUpperCase && isLowerCase == false && isNumeric == false && isSpecial == false) {
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * upperCase.length);
+      password += upperCase.substring(random, random + 1);
+      password = password.substring(0, characterLength);
+      console.log(password);
+    }
+  }
+
+  // if ONLY numbers are selected
   if (isNumeric && isUpperCase == false && isLowerCase == false && isSpecial == false) {
     for (let i = 0; i <= characterLength; i++) {
       var random = Math.floor(Math.random() * numbers.length);
@@ -39,6 +52,18 @@ function password() {
     }
   }
 
+  // if ONLY Special Characters are selected
+  if (isSpecial && isUpperCase == false && isLowerCase == false && isNumeric == false) {
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * specialCharacters.length);
+      password += specialCharacters.substring(random, random + 1);
+      password = password.substring(0, characterLength);
+      console.log(password);
+    }
+  }
+
+
+  // if ALL options are selected
   if (isLowerCase && isUpperCase && isNumeric && isSpecial) {
     for (let i = 0; i <= characterLength; i++) {
       var random = Math.floor(Math.random() * characters.length);
@@ -49,17 +74,17 @@ function password() {
   }
 }
 
-password();
-// let generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-//   let password = generatePassword();
-//   let passwordText = document.querySelector("#password");
+let generateBtn = document.querySelector("#generate");
 
-//   passwordText.value = password;
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
-// }
+  passwordText.value = password;
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
