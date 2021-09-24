a9bbd5db4b97f22417d56d0a3b11e7c3901892a8
maxh <maxhumpherys@gmail.com>
2021-09-23 20:50:55 -0600

    added for loop for ONLY lowercase option

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // lowercase 0-26   uppercase 26-52     numbers 52-62     special characters 62-89
 let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};':<>,./?";
 let numbers = "1234567890";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let SpecialCharacters = "!@#$%^&*()_+-=[]{};':<>,./?";
 
 function password() {
   let characterLength = window.prompt("How many characters would you like?");
@@ -17,6 +20,15 @@ function password() {
   let isUpperCase = confirm("Would you like to include Uppercase characters?");
   let isNumeric = confirm("Would you like to include Numeric characters?");
   let isSpecial = confirm("Would you like to include Special Characters?");
+
+  if (isLowerCase && isUpperCase == false && isNumeric == false && isSpecial == false) {
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * lowerCase.length);
+      password += lowerCase.substring(random, random + 1);
+      password = password.substring(0, characterLength);
+      console.log(password);
+    }
+  }
 
   if (isNumeric && isUpperCase == false && isLowerCase == false && isSpecial == false) {
     for (let i = 0; i <= characterLength; i++) {
