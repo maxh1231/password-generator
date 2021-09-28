1ef257d3ae4400753a33d0fce1061dc45768815f
maxh <maxhumpherys@gmail.com>
2021-09-27 23:51:19 -0600

    added several functions to support if the user selects 2/3 options, instead of 1 or all options

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let specialCharacters = "!@#$%^&*()_+-=[]{};':<>,./?";
 
+
+
 function generatePassword() {
   let characterLength = window.prompt("How many characters would you like?");
   characterLength = parseInt(characterLength, 10);
@@ -13,8 +15,10 @@ function generatePassword() {
   let randomPass = "";
 
 
-  while (characterLength < 8 || characterLength > 128) {
-    characterLength = window.prompt("Pleasse chose between 8 and 128 characters");
+
+
+  while (characterLength < 8 || characterLength > 128 || isNaN(characterLength)) {
+    characterLength = window.prompt("Please chose between 8 and 128 characters");
   }
 
   let isLowerCase = confirm("Would you like to include Lowercase characters?");
@@ -33,6 +37,68 @@ function generatePassword() {
     }
   }
 
+  // if ONLY lowercase & Uppercase is selected
+  if (isLowerCase && isUpperCase && isNumeric == false && isSpecial == false) {
+    var isLowerAndUpper = lowerCase + upperCase;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * isLowerAndUpper.length);
+      randomPass += isLowerAndUpper.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+    }
+  }
+
+  // if ONLY Lowercase & Numeric is selected
+  if (isLowerCase && isUpperCase == false && isNumeric && isSpecial == false) {
+    var isLowerAndNumeric = lowerCase + numbers;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * isLowerAndNumeric.length);
+      randomPass += isLowerAndNumeric.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+    }
+  }
+
+  // if ONLY Lowercase & Special is selected
+  if (isLowerCase && isUpperCase == false && isNumeric == false && isSpecial) {
+    var isLowerAndSpecial = lowerCase + specialCharacters;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * isLowerAndSpecial.length);
+      randomPass += isLowerAndSpecial.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+    }
+  }
+
+  // if ONLY Lowercase & Uppercase & Numeric is selected
+  if (isLowerCase && isUpperCase && isNumeric && isSpecial == false) {
+    var IsLowerAndUpperAndNumeric = lowerCase + upperCase + numbers;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * IsLowerAndUpperAndNumeric.length);
+      randomPass += IsLowerAndUpperAndNumeric.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+    }
+  }
+
+  // if ONLY Lowercase & Uppercase & Special is selected
+  if (isLowerCase && isUpperCase && isNumeric == false && isSpecial) {
+    var IsLowerAndUpperAndSpecial = lowerCase + upperCase + specialCharacters;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * IsLowerAndUpperAndSpecial.length);
+      randomPass += IsLowerAndUpperAndSpecial.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+    }
+  }
+
+  // if ONLY Lowercase & Numbers & Special is selected
+  if (isLowerCase && isUpperCase == false && isNumeric && isSpecial) {
+    var IsLowerAndNumericAndSpecial = lowerCase + numbers + specialCharacters;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * IsLowerAndNumericAndSpecial.length);
+      randomPass += IsLowerAndNumericAndSpecial.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+    }
+  }
+
+
+
   // if ONLY Uppercase characters are selected
   if (isUpperCase && isLowerCase == false && isNumeric == false && isSpecial == false) {
     for (let i = 0; i <= characterLength; i++) {
@@ -43,11 +109,55 @@ function generatePassword() {
     }
   }
 
+  // if ONLY Uppercase & Numeric are selected
+  if (isUpperCase && isLowerCase == false && isNumeric && isSpecial == false) {
+    var isUpperAndNumeric = upperCase + numbers;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * isUpperAndNumeric.length);
+      randomPass += isUpperAndNumeric.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+
+    }
+  }
+
+  // if ONLY Uppercase & Special are selected
+  if (isUpperCase && isLowerCase == false && isNumeric == false && isSpecial) {
+    var isUpperAndSpecial = upperCase + specialCharacters;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * isUpperAndSpecial.length);
+      randomPass += isUpperAndSpecial.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+
+    }
+  }
+
+  // if ONLY Uppercase & Numeric & Special are selected
+  if (isUpperCase && isLowerCase == false && isNumeric && isSpecial) {
+    var isUpperAndNumericAndSpecial = upperCase + numbers + specialCharacters;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * isUpperAndNumericAndSpecial.length);
+      randomPass += isUpperAndNumericAndSpecial.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+
+    }
+  }
+
   // if ONLY numbers are selected
   if (isNumeric && isUpperCase == false && isLowerCase == false && isSpecial == false) {
     for (let i = 0; i <= characterLength; i++) {
       var random = Math.floor(Math.random() * numbers.length);
       randomPass += numbers.substring(random, random + 1);
+      randomPass = randomPass.substring(0, characterLength);
+
+    }
+  }
+
+  // if ONLY numbers & Special are selected
+  if (isNumeric && isUpperCase == false && isLowerCase == false && isSpecial) {
+    var isNumericAndSpecial = numbers + specialCharacters;
+    for (let i = 0; i <= characterLength; i++) {
+      var random = Math.floor(Math.random() * isNumericAndSpecial.length);
+      randomPass += isNumericAndSpecial.substring(random, random + 1);
       randomPass = randomPass.substring(0, characterLength);
 
     }
@@ -74,6 +184,13 @@ function generatePassword() {
 
     }
   }
+
+  // if NONE
+  if (!isLowerCase && !isUpperCase && !isNumeric && !isSpecial) {
+    alert("Please choose atleast 1 option");
+    generatePassword();
+  }
+
   return randomPass;
   console.log(randomPass);
 }
